aeb59f64757d8affab83fd4a74fe0ff6fadbfeeb
Srijal Prasad <srijalprasad@gmail.com>
2024-07-06 16:36:22 +0530

    Add files via upload

---
+++ Day37/eventloop.js
@@ -0,0 +1,23 @@
+console.log('Start');
+
+setTimeout(() => {
+  console.log('setTimeout');
+}, 0);
+
+setImmediate(() => {
+  console.log('setImmediate');
+});
+
+process.nextTick(() => {
+  console.log('nextTick');
+});
+
+console.log('End');
+
+// Output
+Start
+End
+nextTick
+setImmediate
+setTimeout
+
